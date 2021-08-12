a4942d61c1e4df0148a5e943382568f857a78c44
Minoru Takayama <takaya030@yahoo.co.jp>
2021-08-12 17:55:49 +0900

    modify pocket config

--- config/pocket.php
+++ config/pocket.php
@@ -11,5 +11,9 @@ return [
 	'client_id'     => env('POCKET_CLIENT_ID'),
 	'access_token' 	=> env('POCKET_ACCESS_TOKEN'),
 
+	'items_count' 	=> env('POCKET_ITEMS_COUNT'),
+	'keep_tag' 		=> env('POCKET_KEEP_TAG'),
+	'kept_tag' 		=> env('POCKET_KEPT_TAG'),
+
 ];
 
