ef7d894f26eaf7de55ffa57265e5f8bc02e2498e
Minoru Takayama <takaya030@yahoo.co.jp>
2021-08-12 14:29:50 +0900

    add hatenabookmark config

---
+++ config/hatenabookmark.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| HatenaBookmark Config
+	|--------------------------------------------------------------------------
+	*/
+
+	'access_token' 	=> env('HATENA_ACCESS_TOKEN'),
+	'access_token_secret' 	=> env('HATENA_ACCESS_TOKEN_SECRET'),
+
+];
