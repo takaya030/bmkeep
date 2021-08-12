bfb319c0b6a55b912b13e692b04a7ca9e89926a6
Minoru Takayama <takaya030@yahoo.co.jp>
2021-08-12 17:57:16 +0900

    add pocket item

---
+++ app/Models/Pocket/Item.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Models\Pocket;
+
+/**
+ * parser of Pocket post item
+ */
+class Item
+{
+	protected	$item_id;
+	protected	$url;		// resolved url (source url)
+	protected	$tags;
+
+
+    /**
+     * @param mixed $list_item [require] A Pocket post item that json decoed.
+     */
+	public function __construct( $list_item )
+	{
+		$this->parse( $list_item );
+		$this->replace_tag( config('pocket.keep_tag'), config('pocket.kept_tag') );
+	}
+
+	private function parse( $list_item )
+	{
+		$this->item_id = $list_item->item_id;
+
+		$this->url = property_exists( $list_item, "resolved_url" ) ?
+			$list_item->resolved_url : 
+			(property_exists( $list_item, "given_url" ) ? $list_item->given_url : '');
+
+		$this->tags = [];
+		if( !empty($list_item->tags) )
+		{
+			foreach( $list_item->tags as $tag => $data )
+			{
+				$this->tags[] = $tag;
+			}
+		}
+	}
+
+	protected function replace_tag( string $before, string $after )
+	{
+		$this->tags = str_replace( $before, $after, $this->tags );
+	}
+
+	public function get_param_post_hatena()
+	{
+		$url_str = 'url=' . $this->url;
+		if( !empty( $this->tags ) )
+		{
+			$tags_str = '&comment=';
+			foreach( $this->tags as $tag )
+			{
+				$tags_str .= "[{$tag}]";
+			}
+			$url_str .= $tags_str;
+		}
+		return $url_str;
+	}
+
+	public function get_param_tags_replace()
+	{
+		return [
+			'item_id' => $this->item_id,
+			'tags' => $this->tags,
+		];
+	}
+}
