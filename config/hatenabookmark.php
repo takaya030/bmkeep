25a7a61d8d97a53389fff506e8ef01ffd585e838
Minoru Takayama <takaya030@yahoo.co.jp>
2022-07-10 17:01:16 +0900

    modify config of Hatenabookmark

--- config/hatenabookmark.php
+++ config/hatenabookmark.php
@@ -8,6 +8,8 @@ return [
 	|--------------------------------------------------------------------------
 	*/
 
+	'client_id' => env('HATENA_CLIENT_ID'),
+	'client_secret' => env('HATENA_CLIENT_SECRET'),
 	'access_token' 	=> env('HATENA_ACCESS_TOKEN'),
 	'access_token_secret' 	=> env('HATENA_ACCESS_TOKEN_SECRET'),
 
