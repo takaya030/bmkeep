775c26eb8f0059e7aad620faa3b20cc956edd290
Minoru Takayama <takaya030@yahoo.co.jp>
2022-07-10 17:03:31 +0900

    add LeagueOAuthService of Hatenabookmark

---
+++ app/Models/OAuth/League/Hatenabookmark.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Models\OAuth\League;
+
+use League\OAuth1\Client\Server\Server;
+use League\OAuth1\Client\Credentials\TokenCredentials;
+use LogicException;
+use RuntimeException;
+
+class Hatenabookmark extends Server
+{
+    /**
+     * @inheritDoc
+     */
+    public function urlTemporaryCredentials()
+    {
+        return 'https://www.hatena.com/oauth/initiate?scope=read_public%2Cwrite_public';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function urlAuthorization()
+    {
+        return 'https://www.hatena.ne.jp/oauth/authorize';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function urlTokenCredentials()
+    {
+        return 'https://www.hatena.com/oauth/token';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function urlUserDetails()
+    {
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function userDetails($data, TokenCredentials $tokenCredentials)
+    {
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function userUid($data, TokenCredentials $tokenCredentials)
+    {
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function userEmail($data, TokenCredentials $tokenCredentials)
+    {
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function userScreenName($data, TokenCredentials $tokenCredentials)
+    {
+        return null;
+    }
+}
