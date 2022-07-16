e851dc66c9dab31eb9170c294fd59d9e170d3291
Minoru Takayama <takaya030@yahoo.co.jp>
2022-07-16 22:02:54 +0900

    remove unused actions

--- app/Http/Controllers/PocketController.php
+++ app/Http/Controllers/PocketController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use OAuth\Common\Http\Exception\TokenResponseException;
-
 use Illuminate\Http\Request;
 
 use App\Models\Pocket\Client as PocketClient;
 use App\Models\Pocket\Item as PocketItem;
 use App\Models\HatenaBookmark\LeagueOAuthClient as HatenaClient;
+use Throwable;
 
 class PocketController extends Controller
 {
@@ -43,64 +42,12 @@ class PocketController extends Controller
 				return response()->json([ 'access_token' => $access_token ]);
 			}
 		}
-		catch( TokenResponseException $e ) {
+		catch( Throwable $e ) {
 			return response()->json([ 'error' => $e->getMessage() ]);
 		}
 
 		return response()->json([ 'error' => 'Invalid result.' ]);
 
-	}
-
-	public function loginOAuthOld(Request $request)
-	{
-		// get pocket service
-		$pocketService = \OAuth::consumer('Pocket',url('/loginresult'));
-
-		// get request token
-		$reqToken = $pocketService->requestRequestToken();
-		$request->session()->put( 'code', $reqToken );
-
-		// get pocketService authorization
-		$url = $pocketService->getAuthorizationUri([ 'request_token' => $reqToken ]);
-
-		// return to pocket login url
-		return redirect((string)$url);
-	}
-
-	public function loginResult(Request $request)
-	{
-		// get data from request
-		$code = $request->session()->get('code');
-
-		// get pocket service
-		$pocketService = \OAuth::consumer('Pocket',url('/loginresult'));
-
-		// if code is provided get user data and sign in
-		if ( ! is_null($code))
-		{
-			// This was a callback request from pocket, get the token
-			$token = $pocketService->requestAccessToken($code);
-
-			$params = [
-				'consumer_key'	=> config('pocket.client_id'),
-				'access_token'	=> $token->getAccessToken(),
-				'sort' => 'newest',
-				'count' => '1',
-				'detailType'	=> 'complete',
-			];
-
-			//dd($params);
-
-			// get a post (test)
-			$json_result = $pocketService->request('https://getpocket.com/v3/get', 'POST', json_encode($params), [ 'Content-Type' => 'application/json' ]);
-			$result = json_decode( $json_result );
-
-			dd($result);
-		}
-		else
-		{
-			dd('code is null.');
-		}
 	}
 
 	public function getRetrieve(Request $request)
@@ -144,7 +91,7 @@ class PocketController extends Controller
 
 			return response()->json($tags_result);
 		}
-		catch( TokenResponseException $e ) {
+		catch( Throwable $e ) {
 			return response()->json([ 'error' => $e->getMessage() ]);
 		}
 
@@ -185,7 +132,7 @@ class PocketController extends Controller
 
 			return response()->json($delete_result);
 		}
-		catch( TokenResponseException $e ) {
+		catch( Throwable $e ) {
 			return response()->json([ 'error' => $e->getMessage() ]);
 		}
 	}
