001106caa24c10bfcaa79a42ec323d768afb87d8
Minoru Takayama <takaya030@yahoo.co.jp>
2022-12-30 07:13:59 +0000

    add rss item class

---
+++ app/Models/HatenaBookmark/NewsItem.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models\HatenaBookmark;
+
+use \Carbon\Carbon;
+use \SimplePie\Item;
+
+class NewsItem
+{
+    protected	$title;
+	protected	$url;
+	protected	$date;
+	protected	$timestamp;
+
+	/**
+	 * @param \SimplePie_Item $item
+	 */
+	public function __construct( Item $item )
+	{
+		$this->title	= $item->get_title();	// news title
+		$this->url		= $item->get_link();	// news url
+		$this->date		= $item->get_date('Y-m-d H:i:s T');	// posting date of news
+		$this->timestamp	= Carbon::createFromFormat( 'Y-m-d H:i:s T', $item->get_date('Y-m-d H:i:s T') )->timestamp;	// posting timestamp of news
+	}
+
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+	public function getUrl()
+	{
+		return $this->url;
+	}
+
+	public function getDate()
+	{
+		return $this->date;
+	}
+
+	public function getTimestamp()
+	{
+		return $this->timestamp;
+	}
+}
