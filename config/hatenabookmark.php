d2942af644f16f52e52ec998106e403e8ee90dba
Minoru Takayama <takaya030@yahoo.co.jp>
2023-05-03 03:31:03 +0000

    add configurations for read it later

--- config/hatenabookmark.php
+++ config/hatenabookmark.php
@@ -13,4 +13,10 @@ return [
 	'access_token' 	=> env('HATENA_ACCESS_TOKEN'),
 	'access_token_secret' 	=> env('HATENA_ACCESS_TOKEN_SECRET'),
 
+	// for "read it later"
+	'ril_valid_itmes' 	=> env('RIL_VALID_ITEMS'),
+	'ril_items_in_page' => env('RIL_ITEMS_IN_PAGE'),
+	'ril_max_delete_items' => env('RIL_MAX_DELETE_ITEMS'),
+	'ril_feed_url' => env('RIL_FEED_URL'),
+
 ];
